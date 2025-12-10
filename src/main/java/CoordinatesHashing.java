5c4afb718d135ba9ddf295638cec90ac84436395
Andrew Chen <andrew.chen@displayr.com>
2025-12-10 11:04:24 +1030

    Advent of Code Day 9 Optimized down to 96ms

---
+++ src/main/java/CoordinatesHashing.java
@@ -0,0 +1,36 @@
+package main.java;
+
+public class CoordinatesHashing {
+    // Strong 64-bit mixing function (MurmurHash3 finalizer)
+    private static long mix64(long z) {
+        z ^= (z >>> 33);
+        z *= 0xff51afd7ed558ccdL;
+        z ^= (z >>> 33);
+        z *= 0xc4ceb9fe1a85ec53L;
+        z ^= (z >>> 33);
+        return z;
+    }
+
+    // Hash a single int vec2
+    public static long hashVec2(int x, int y) {
+        long h = 0x9E3779B97F4A7C15L; // golden ratio seed
+        h ^= mix64(x);
+        h = mix64(h);
+        h ^= mix64(y);
+        h = mix64(h);
+        return h;
+    }
+
+    // Order-independent hash of two int vec2
+    public static long hashUnordered(int ax, int ay, int bx, int by) {
+        long ha = hashVec2(ax, ay);
+        long hb = hashVec2(bx, by);
+
+        long lo = Math.min(ha, hb);
+        long hi = Math.max(ha, hb);
+
+        long h = lo;
+        h ^= hi + 0x9E3779B97F4A7C15L + (h << 6) + (h >>> 2); // boost::hash_combine style
+        return mix64(h);
+    }
+}
--- src/main/java/LargestRectangle.java
+++ src/main/java/LargestRectangle.java
@@ -4,10 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class LargestRectangle {
 
@@ -79,65 +76,127 @@ public class LargestRectangle {
             var pos = node.coordinates.get(dimension);
             return this.A.coordinates.get(dimension) == pos || this.B.coordinates.get(dimension) == pos;
         }
+    }
 
-        public boolean trySetOptionalNode(Node optionalNode) {
-            if (this.withinDimension(0, optionalNode) && this.withinDimension(1, optionalNode)) {
-                this.OptionalNode = optionalNode;
-                return true;
+    // A class to define an outside perimeter of the shape efficiently
+    class OutsidePerimeter {
+        HashMap<Long, TreeSet<Long>> rowToColValues = new HashMap<>();
+        HashMap<Long, TreeSet<Long>> colToRowValues = new HashMap<>();
+
+        // This takes all the values 1 block outside the perimeter to construct a perimeter of values we can use to check
+        // when a testing rectangles perimeter hits these values we know it has gone out of the defined shape.
+        // Limitations:
+        // - The shape defined by the perimeter must be one single shape the path of the shape cannot cross over
+        // - The perimeter + 1 does not include corner block this is not neccessary because we know that since we are making
+        //   rectangles the perimeters are guaranteed to be orthogonal to any red squares which are also used to define the path.
+        public OutsidePerimeter(List<Node> data, int direction) {
+            var newDirection = 0L;
+            var point0 = data.get(data.size() - 1).coordinates;
+            var point1 = data.get(0).coordinates;
+            for (var i = 1; i < data.size() + 1; i++) {
+                var j = i % data.size();
+                var point2 = data.get(j).coordinates;
+                var dimension = point2.get(0) - point1.get(0) == 0 ? 1 : 0;
+
+                var increment = (point2.get(dimension) - point1.get(dimension)) / Math.abs((point2.get(dimension) - point1.get(dimension)));
+                var outsideIncrement = increment * direction * -1;
+                for (var a = point1.get(dimension); a != point2.get(dimension) + increment; a += increment) {
+                    // Increments the perimeter by 1 in the outside direction
+                    if (dimension == 0) {
+                        addValue(a, point1.get(1) + outsideIncrement);
+                    } else {
+                        addValue(point1.get(0) + outsideIncrement * -1, a);
+                    }
+                }
+
+                // On the inside of an L bend
+                newDirection = getVectorScalarProductFromPoints(new Node(point0), new Node(point1), new Node(point2));
+                if (newDirection != direction) {
+                    if (dimension == 0) {
+                        var y = (increment > 0 ? increment : -increment) * direction;
+                        removeValue(point1.get(0) + increment, point1.get(1));
+                        removeValue(point1.get(0), point1.get(1) + y);
+                    }else {
+                        var x = (increment > 0 ? -increment : increment) * direction;
+                        removeValue(point1.get(0) + x, point1.get(1));
+                        removeValue(point1.get(0), point1.get(1) + increment);
+                    }
+                }
+                point0 = point1;
+                point1 = point2;
+            }
+        }
+
+        public void addValue(long x, long y) {
+            if (rowToColValues.containsKey(x)) {
+                rowToColValues.get(x).add(y);
+            } else {
+                var newHashSetCols = new TreeSet<Long>();
+                newHashSetCols.add(y);
+                rowToColValues.put(x, newHashSetCols);
+            }
+
+            if (colToRowValues.containsKey(y)) {
+                colToRowValues.get(y).add(x);
+            } else {
+                var newHashSetRows = new TreeSet<Long>();
+                newHashSetRows.add(x);
+                colToRowValues.put(y, newHashSetRows);
+            }
+        }
+
+        public void removeValue(long x, long y) {
+            if (rowToColValues.containsKey(x)) {
+                rowToColValues.get(x).remove(y);
+                if (rowToColValues.get(x).size() == 0) {
+                    rowToColValues.remove(x);
+                }
+            }
+
+            if (rowToColValues.containsKey(y)) {
+                colToRowValues.get(y).remove(x);
+                if (colToRowValues.get(y).size() == 0) {
+                    colToRowValues.remove(y);
+                }
+            }
+        }
+
+        private boolean withinRange(long min, long max, long num) {
+            return min <= num && max >= min;
+        }
+
+        private boolean collidesWithLine(boolean isXDimension, long index, long min, long max) {
+            // Gets a sorted list of indexes
+            var dimensionalSet = isXDimension ? rowToColValues : colToRowValues;
+            if (!dimensionalSet.containsKey(index)) {
+                return false;
+            }
+
+            var rowsOrCols = dimensionalSet.get(index);
+            for(var idx : rowsOrCols) {
+                // Escape early since the iterations are sorted
+                if (idx > max) {
+                    break;
+                }
+                if (withinRange(min, max, idx)) {
+                    return true;
+                }
             }
             return false;
         }
 
+        public boolean collidesWithPerimeter(Rectangle rectangle) {
+            var minX = Math.min(rectangle.A.getX(), rectangle.B.getX());
+            var maxX = Math.max(rectangle.A.getX(), rectangle.B.getX());
+            var minY = Math.min(rectangle.A.getY(), rectangle.B.getY());
+            var maxY = Math.max(rectangle.A.getY(), rectangle.B.getY());
 
-
-        public Rectangle tryExpandRectangle(Node newNode) {
-            if ((this.A.coordinates.get(0) == newNode.coordinates.get(0) && withinDimension(1, newNode)) ||
-                    (this.A.coordinates.get(1) == newNode.coordinates.get(1) && withinDimension(0, newNode))) {
-                var tempRect = this.B.createRectangle(newNode);
-                if (tempRect.area > area) {
-                    this.A = newNode;
-                    area = tempRect.area;
-                    return tempRect;
-                }
-            } else if ((this.B.coordinates.get(0) == newNode.coordinates.get(0) && withinDimension(1, newNode)) ||
-                    (this.B.coordinates.get(1) == newNode.coordinates.get(1) && withinDimension(0, newNode))) {
-                var tempRect = this.A.createRectangle(newNode);
-                if (tempRect.area > area) {
-                    this.B = newNode;
-                    area = tempRect.area;
-                    return tempRect;
-                }
-            }
-            if (OptionalNode != null) {
-                var tempRect = this.OptionalNode.createRectangle(newNode);
-                if (tempRect.area > area && (equalsDimension(0, newNode) || equalsDimension(1, newNode))) {
-                    this.A = OptionalNode;
-                    this.B = newNode;
-                    return tempRect;
-                }
-            }
-
-            return this;
-        }
-
-        public Rectangle tryExpandOptionalNode(Node newNode) {
-            if (OptionalNode != null) {
-                var tempRect = this.OptionalNode.createRectangle(newNode);
-                if (tempRect.area > area && (equalsDimension(0, newNode) || equalsDimension(1, newNode))) {
-                    return tempRect;
-                }
-            }
-            return null;
-        }
-
-        public List<Rectangle> getPossibleRectangles(Node newNode) {
-            var result = new ArrayList<Rectangle>();
-
-            result.add(this.A.createRectangle(newNode));
-            result.add(this.B.createRectangle(newNode));
-            if (OptionalNode != null) {
-                result.add(this.OptionalNode.createRectangle(newNode));
-            }
+            // Test the four lines that make the perimeter of the rectangle to see if
+            // we have any outer perimeter collisions
+            var result = collidesWithLine(true, minX, minY, maxY) ||
+                    collidesWithLine(true, maxX, minY, maxY) ||
+                    collidesWithLine(false, minY, minX, maxX) ||
+                    collidesWithLine(false, maxY, minX, maxX);
             return result;
         }
     }
@@ -149,10 +208,12 @@ public class LargestRectangle {
             this.coordinates = coordinates;
         }
 
+        public Long getX() {
+            return this.coordinates.get(0);
+        }
 
-        public Rectangle createRectangle(Node node) {
-            var distance = (long)(Math.abs(coordinates.get(0) - node.coordinates.get(0)) + 1) * (long)(Math.abs(coordinates.get(1) - node.coordinates.get(1)) + 1);
-            return new Rectangle(this, node, distance);
+        public Long getY() {
+            return this.coordinates.get(1);
         }
     }
 
@@ -192,7 +253,7 @@ public class LargestRectangle {
 
                 // Here we use the hash code to check if the Edge already exists in the map regardless of direction A->B or B->A
                 if (!hashCodeCheck.contains(tempEdge.hashCode())) {
-                    var edge = node.createRectangle(comparisonNode);
+                    var edge = new Rectangle(node, comparisonNode);
                     graph.add(edge);
                     hashCodeCheck.add(edge.hashCode());
                 }
@@ -215,13 +276,19 @@ public class LargestRectangle {
         return (long)sortedGraph.get(sortedGraph.size() - 1).area;
     }
 
-    public List<Long> subtractPositions(List<Long> pos1, List<Long> pos2) {
-        return List.of(pos2.get(0) - pos1.get(0), pos2.get(1) - pos1.get(1));
+    // Convert our positional coordinates to a vector from point1 to point2
+    public List<Long> getVectorFromPositions(Node pos1, Node pos2) {
+        return List.of(pos2.getX()- pos1.getX(), pos2.getY() - pos1.getY());
     }
 
-    public Long direction(List<Long> pos1, List<Long> pos2, List<Long> pos3) {
-        var v1 = subtractPositions(pos1, pos2);
-        var v2 = subtractPositions(pos2, pos3);
+    // When given 3 points in order we convert them into 2 vectors
+    // By computing the scalar product of the two vectors we can determine which direction the second vector is
+    // relative the first.
+    // If the value is positive it means the second vector is to the right of the first which indicates a right turn/bend
+    // If the value is negative it means the second vector is to the left indicating a left turn/bend
+    public Long getVectorScalarProductFromPoints(Node pos1, Node pos2, Node pos3) {
+        var v1 = getVectorFromPositions(pos1, pos2);
+        var v2 = getVectorFromPositions(pos2, pos3);
         var direction = v1.get(0) * v2.get(1) - v1.get(1) * v2.get(0);
         if (direction == 0) {
             return 0L;
@@ -229,157 +296,58 @@ public class LargestRectangle {
         return direction / Math.abs(direction);
     }
 
+    // This find the direction of the path which we know to be a loop
+    // if it is +1 it means the path rotates clockwise otherwise -1 is anticlockwise
+    public int getDirectionOfPath(List<Node> data) {
+        var point0 = data.get(data.size() - 1);
+        var point1 = data.get(0);
+        var direction = 0;
+        for (var i = 1; i < data.size() + 1; i++) {
+            var j = i % data.size();
+            var point2 = data.get(j);
+            direction += getVectorScalarProductFromPoints(point0, point1, point2);
+            point0 = point1;
+            point1 = point2;
+        }
 
-    public String coordinatesToHashCode(Long x, Long y) {
-        return (x + "," + y);
-    }
-
-    // Strong 64-bit mixing function (MurmurHash3 finalizer)
-    private static long mix64(long z) {
-        z ^= (z >>> 33);
-        z *= 0xff51afd7ed558ccdL;
-        z ^= (z >>> 33);
-        z *= 0xc4ceb9fe1a85ec53L;
-        z ^= (z >>> 33);
-        return z;
-    }
-
-    // Hash a single int vec2
-    public static long hashVec2(int x, int y) {
-        long h = 0x9E3779B97F4A7C15L; // golden ratio seed
-        h ^= mix64(x);
-        h = mix64(h);
-        h ^= mix64(y);
-        h = mix64(h);
-        return h;
-    }
-
-    // Order-independent hash of two int vec2
-    public static long hashUnordered(int ax, int ay, int bx, int by) {
-        long ha = hashVec2(ax, ay);
-        long hb = hashVec2(bx, by);
-
-        long lo = Math.min(ha, hb);
-        long hi = Math.max(ha, hb);
-
-        long h = lo;
-        h ^= hi + 0x9E3779B97F4A7C15L + (h << 6) + (h >>> 2); // boost::hash_combine style
-        return mix64(h);
-    }
-
-    public void test() {
-        var point1 = List.of(0L,0L);
-        var increment = 1;
-        var direction = 1;
-        var outsideIncrement = increment * direction * -1;
-        var dimension = 1;
-        var x = dimension == 0 ? increment : outsideIncrement;
-        var y = dimension == 0 ? outsideIncrement : increment;
-
-        var straight = coordinatesToHashCode(point1.get(0) + x, point1.get(1));
-        var right = coordinatesToHashCode(point1.get(0), point1.get(1) + y);
+        // if -1 the path turns to the left if 1 the path turns to the right making a loop
+        direction = direction / Math.abs(direction);
+        return direction;
     }
 
     public Long solvePart2() {
-        var data = LoadTextFile();
-        var perimeter = new HashSet<String>();
-        var direction = 0L;
-
-        var point0 = data.get(data.size() - 1);
-        var point1 = data.get(0);
-        for (var i = 1; i < data.size() + 1; i++) {
-            var j = i % data.size();
-            var point2 = data.get(j);
-            direction += direction(point0, point1, point2);
-            point0 = point1;
-            point1 = point2;
-        }
-
-        direction = direction / Math.abs(direction);
-
-        var newDirection = 0L;
-        point0 = data.get(data.size() - 1);
-        point1 = data.get(0);
-        for (var i = 1; i < data.size() + 1; i++) {
-            var j = i % data.size();
-            var point2 = data.get(j);
-            var dimension = point2.get(0) - point1.get(0) == 0 ? 1 : 0;
-
-            var increment = (point2.get(dimension) - point1.get(dimension)) / Math.abs((point2.get(dimension) - point1.get(dimension)));
-            var outsideIncrement = increment * direction * -1;
-            for (var a = point1.get(dimension); a != point2.get(dimension) + increment; a += increment) {
-                // Increments the perimeter by 1 in the outside direction
-                if (dimension == 0) {
-                    perimeter.add(coordinatesToHashCode(a, point1.get(1) + outsideIncrement));
-                } else {
-                    perimeter.add(coordinatesToHashCode(point1.get(0) + outsideIncrement * -1, a));
-                }
-            }
-
-            // On the inside of an L bend
-            newDirection = direction(point0, point1, point2);
-            if (newDirection != direction) {
-                if (dimension == 0) {
-                    var y = (increment > 0 ? increment : -increment) * direction;
-                    var xDir = coordinatesToHashCode(point1.get(0) + increment, point1.get(1));
-                    var yDir = coordinatesToHashCode(point1.get(0), point1.get(1) + y);
-                    perimeter.remove(xDir);
-                    perimeter.remove(yDir);
-                }else {
-                    var x = (increment > 0 ? -increment : increment) * direction;
-                    var xDir = coordinatesToHashCode(point1.get(0) + x, point1.get(1));
-                    var yDir = coordinatesToHashCode(point1.get(0), point1.get(1) + increment);
-                    perimeter.remove(xDir);
-                    perimeter.remove(yDir);
-                }
-            }
-            point0 = point1;
-            point1 = point2;
-        }
+        var data = LoadTextFile().stream().map(c -> new Node(c)).toList();
+        var direction = getDirectionOfPath(data);
+        var outsidePerimeter = new OutsidePerimeter(data, direction);
 
         var largestArea = 0L;
         var seenPoints =  new HashSet<Long>();
-        var count = 0.0;
-        double durationSeconds = 0;
+        long start = System.nanoTime();
         for(var point: data) {
-            count++;
-            if (count % 5 == 0) {
-                System.out.println("Completion:\t" + (count / data.size() * 100) + "%");
-                System.out.println("Time to completion:\t" + (durationSeconds * (data.size() - count)) + "s");
-            }
-            long start = System.nanoTime();
             for(var point2: data) {
-                if (seenPoints.contains(hashUnordered(point.get(0).intValue(), point.get(1).intValue(), point2.get(0).intValue(), point2.get(1).intValue()))) {
+                var coordinatesHashCode = CoordinatesHashing.hashUnordered(point.getX().intValue(), point.getY().intValue(), point2.getX().intValue(), point2.getY().intValue());
+                if (seenPoints.contains(coordinatesHashCode)) {
                     continue;
                 }
-                var tempRect = new Rectangle(new Node(point), new Node(point2));
+                var tempRect = new Rectangle(point, point2);
                 if (tempRect.area <= largestArea) {
                     continue;
                 }
-                var directionX = point2.get(0) - point.get(0) == 0 ? 1 : (point2.get(0) - point.get(0)) / Math.abs((point2.get(0) - point.get(0)));
-                var directionY = point2.get(1) - point.get(1) == 0 ? 1 : (point2.get(1) - point.get(1)) / Math.abs((point2.get(1) - point.get(1)));
-                var hitPerimeter = false;
-                for (var x = point.get(0); x != point2.get(0) + directionX && !hitPerimeter; x += directionX) {
-                    hitPerimeter = hitPerimeter || perimeter.contains(coordinatesToHashCode(x, point.get(1)));
-                    hitPerimeter = hitPerimeter || perimeter.contains(coordinatesToHashCode(x, point2.get(1)));
-                }
 
-                for (var y = point.get(1); y != point2.get(1) + directionY && !hitPerimeter; y += directionY) {
-                    hitPerimeter = hitPerimeter || perimeter.contains(coordinatesToHashCode(point.get(0), y));
-                    hitPerimeter = hitPerimeter || perimeter.contains(coordinatesToHashCode(point2.get(0), y));
-                }
+                var hitPerimeter = outsidePerimeter.collidesWithPerimeter(tempRect);
                 if (!hitPerimeter) {
                     largestArea = (long)tempRect.area;
                 }
 
-                seenPoints.add(hashUnordered(point.get(0).intValue(), point.get(1).intValue(), point2.get(0).intValue(), point2.get(1).intValue()));
+                seenPoints.add(coordinatesHashCode);
             }
-            long end = System.nanoTime();
-            long durationNs = end - start;
-            durationSeconds = (durationNs / 1_000_000.0 / 1000);
+
 
         }
-
+        long end = System.nanoTime();
+        long durationNs = end - start;
+        double durationMs = (durationNs / 1_000_000.0);
+        System.out.println("Duration:\t" + durationMs + "ms");
 
 
         return largestArea;
